aa4e21788af04a25e1f32bf6af0a7579ce2be0ff
LeandroMelloo <leandromoreno1909@hotmail.com>
2021-12-27 21:58:07 -0300

    Adicionando o typescript e incrementando a forma de desenvolver

--- estilo_arquitetural_rest_nodejs/src/index.ts
+++ estilo_arquitetural_rest_nodejs/src/index.ts
@@ -1,1 +1,10 @@
-console.log('Executei..')
+import express, { Request, Response, NextFunction } from 'express'; // gerenciador de rotas HTTP
+
+const app = express(); // instanciando o express
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({ foo: 'executando...' });
+});
+
+app.listen(3000, () => console.log('Aplicação executando na porta 3000!'));
+
